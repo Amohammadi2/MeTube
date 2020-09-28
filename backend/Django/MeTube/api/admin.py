44eb2ec867b5695eb15d5f3533ed3a6bc4007138
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2020-09-28 17:37:22 +0330

    fix: some fixes in admin and models

--- backend/Django/MeTube/api/admin.py
+++ backend/Django/MeTube/api/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
+from django import forms
 from .models import *
-# Register your models here.
+# Register your models here
 
-admin.site.register(Profile)
-admin.site.register(Notification)
-admin.site.register(Channel)
-admin.site.register(Video)
-admin.site.register(Subscription)
-admin.site.register(Comment)
+class VideoFormAdmin(forms.ModelForm):
+    description = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+        model = Video
+        exclude = []
+
+class VideoAdmin(admin.ModelAdmin):
+    form = VideoFormAdmin
+
+def register_models():
+    admin.site.register(Profile)
+    admin.site.register(Notification)
+    admin.site.register(Channel)
+    admin.site.register(Video, VideoAdmin)
+    admin.site.register(Subscription)
+    admin.site.register(Comment)
+
+register_models()
--- backend/Django/MeTube/api/models.py
+++ backend/Django/MeTube/api/models.py
@@ -7,12 +7,17 @@ class Profile(models.Model):
     profile_img = models.ImageField(upload_to="profiles/", null=True, blank=True)
     bio = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.user.username + "'s profile"
+
 
 class Channel(models.Model):
     subscribers = models.ManyToManyField(User, through=u"Subscription", related_name="subscriptions")
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
 
 class Video(models.Model):
     channel = models.ForeignKey(Channel, models.CASCADE)
@@ -21,12 +26,18 @@ class Video(models.Model):
     video = models.FileField(upload_to="videos/")
     poster = models.ImageField(upload_to="imgs/")
 
+    def __str__(self):
+        return self.title
+
 class Comment(models.Model):
     # both User and Video models have access to
     # the comments through their `comments` property
     commenter = models.ForeignKey(User, models.CASCADE ,related_name="comments")
     video = models.ForeignKey(Video, models.CASCADE ,related_name="comments")
     comment_text = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.comment_text
 
 class Notification(models.Model):
     status_choices = [
@@ -51,9 +62,9 @@ class Subscription(models.Model):
     receive_notifications = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
-        # if the this is not an `update` process
-        # then automatically set the subscription date
-        # to the current time of the system
         if not self.id:
-            self.subscription_date = now().date
+            self.subscription_date = now().date()
         return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.subscriber.username + " is subscribing  " + self.subscribed_channel.name
